23bda405d47abd3a765c9aab1c7187241c6b6a36
Vasiliy <vasiliy.p.kostin@gmail.com>
2015-12-31 13:59:52 +0000

    search layer api added

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -3,121 +3,209 @@
 var path = process.cwd();
 
 var host = process.env.SERVER_URL;
+var apiKey = process.env.GOOGLE_SEARCH_KEY;
 
-module.exports = function (app, passport) {
+var https = require("https");
+
+module.exports = function(app, passport) {
 
 	app.route('/')
-		.get( function (req, res) {
+		.get(function(req, res) {
 			res.sendFile(path + '/src/public/index.html');
 		});
-		
-		
+
+
 	//microtime service
 	app.route('/api-1')
-		.get( function (req, res) {
+		.get(function(req, res) {
 			res.sendFile(path + '/src/public/api-1.html');
 		});
 	app.route('/api-1/:date')
-		.get(function(req, res){
-		    
-		    var d = req.params.date;
-		    if(d) {
-		        var moment = require('moment');
-		        if(d == parseInt(d)){
-		            var date = moment(d,'X');
-	            } else {
-	                var date = moment(d,'MMMM D, YYYY');
-	            }
-		        res.json({
-		            'unix':date.format('X'),
-		            'natural':date.format('MMMM D, YYYY')
-		        });
-		    } else {
-		        res.end('null');
-		    }
+		.get(function(req, res) {
+
+			var d = req.params.date;
+			if (d) {
+				var moment = require('moment');
+				if (d == parseInt(d)) {
+					var date = moment(d, 'X');
+				}
+				else {
+					var date = moment(d, 'MMMM D, YYYY');
+				}
+				res.json({
+					'unix': date.format('X'),
+					'natural': date.format('MMMM D, YYYY')
+				});
+			}
+			else {
+				res.end('null');
+			}
 		})
-		
+
 	//whoami service
 	app.route('/api-2')
-		.get( function (req, res) {
-		    
-		    
-		    
-		    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-		    var lang =  req.headers['accept-language'].split(',').shift();
-		    if(!lang){
-		        lang = 'en-US';
-		    }
-		    
+		.get(function(req, res) {
+
+
+
+			var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+			var lang = req.headers['accept-language'].split(',').shift();
+			if (!lang) {
+				lang = 'en-US';
+			}
+
 			res.json({
-			    'ipaddress':ip,
-			    'language':lang,
-			    'software':req.useragent.os
-			    
+				'ipaddress': ip,
+				'language': lang,
+				'software': req.useragent.os
+
 			});
 		});
-		
+
 	//URL Shortener
 	var Link = require("../models/link.js");
-	
+
 	app.route('/api-3')
-		.get( function (req, res) {
+		.get(function(req, res) {
 			res.sendFile(path + '/src/public/api-3.html');
 		});
-		
+
 	app.route('/api-3/new/*')
 		.get(function(req, res) {
-		
-		var url = req.url.replace('/api-3/new/','');
-		Link.findOne({'url':url})
-		.exec(function(err, link){
-			
-			if(err) throw err;
-			
-			if(!link){
-				link = new Link();
-				link.url = url;
-				Link.count(function(err,c){
-					
-					if(err) throw err;
-					
-					link.short = c;
-					link.save();
-					handleLink(res, link);
-				})
-				 
-			} else {
-				handleLink(res, link);
-			}
-			
-		})
-		
-		
-	});
-	
-	app.route('/api-3/:id')
-		.get(function(req, res){
-		    
-		    var id = req.params.id;
-		    Link.findOne({'short':id})
-		    .exec(function(err, link) {
-		        
-		        if(err) throw err;
-		        
-		        if(link){
-		        	res.redirect(link.url);
-		        } else {
-		        	res.end('unknown link');
-		        }
-		    });
-		    
-		});
-};
 
-function handleLink(res, link){
-	
+			var url = req.url.replace('/api-3/new/', '');
+			Link.findOne({
+					'url': url
+				})
+				.exec(function(err, link) {
+
+					if (err) throw err;
+
+					if (!link) {
+						link = new Link();
+						link.url = url;
+						Link.count(function(err, c) {
+
+							if (err) throw err;
+
+							link.short = c;
+							link.save();
+							handleLink(res, link);
+						})
+
+					}
+					else {
+						handleLink(res, link);
+					}
+
+				})
+
+
+		});
+
+	app.route('/api-3/:id')
+		.get(function(req, res) {
+
+			var id = req.params.id;
+			Link.findOne({
+					'short': id
+				})
+				.exec(function(err, link) {
+
+					if (err) throw err;
+
+					if (link) {
+						res.redirect(link.url);
+					}
+					else {
+						res.end('unknown link');
+					}
+				});
+
+		});
+
+
+	//search layer
+	app.route('/api-4/')
+		.get(function(req, res) {
+
+
+			var q = req.query.q;
+			var start = parseInt(req.query.start);
+			if(!start){
+				start='';
+			} else {
+				start = '&start=' + (start * 10);
+			}
+
+
+			if (q) {
+				//find books by google API
+				var options = {
+					hostname: 'www.googleapis.com',
+					port: 443,
+					path: '/customsearch/v1?q=' + encodeURIComponent(q) + start+'&count=10&cx=008556373124519553628%3Ab3-4vgdf2ag&searchType=image&key=' + apiKey + '&fields=items/title,items/link,items/image/thumbnailLink,items/image/contextLink',
+
+					method: 'GET'
+				};
+				var reqs = https.request(options, function(r) {
+
+					var data = '';
+
+					r.on('data', function(d) {
+						data += d;
+					});
+
+					r.on('end', function() {
+						var ret = [];
+						data = JSON.parse(data);
+						var ret =[];
+						if (data.items) {
+							data.items.forEach(function(b) {
+								ret.push({
+									'url': b.link,
+									'snippet':b.title,
+									'thumbnail':b.image.thumbnailLink,
+									'context':b.image.contextLink 
+									
+									
+								})
+							});
+							
+							res.json(ret);
+							
+						}
+						res.end();
+					});
+					
+				
+				});
+				
+				reqs.end();
+				reqs.on('error', function(e) {
+				  throw e;
+				});		
+				
+			} else {
+				res.sendFile(path + '/src/public/api-4.html');
+			}
+
+
+
+
+
+
+
+
+
+
+		});
+}
+
+function handleLink(res, link) {
+
 	res.json({
-		'original_url':link.url,
-		'short_url':host+'api-3/'+link.short
+		'original_url': link.url,
+		'short_url': host + 'api-3/' + link.short
 	})
 }
