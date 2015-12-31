5df344cbcd8d3a97038260f40ecdfc2af72504fa
Vasiliy <vasiliy.p.kostin@gmail.com>
2015-12-31 08:55:07 +0000

    port config added

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 'use strict';
 
+require('dotenv').load();
+
 var express = require('express');
 var routes = require('./src/app/routes/index.js');
 
 var app = express();
 
 routes(app);
-var port = 8080;
+
+var port = process.env.PORT || 8080;
+
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
